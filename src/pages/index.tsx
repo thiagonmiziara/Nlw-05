0e5877c1d68ec7ddf07cfdda9087e9094787947b
thiago miziara <thiagonmiziara@gmail.com>
2021-04-20 11:53:36 -0300

    add lib data-fns para formatar data do Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from 'next/head'
+import { Header } from '../components/Header'
 
 export default function Home() {
   return (
     <>
-    <h1>Hello world!</h1>
+    <Header/>
     
    </>
   )
