e18a3e4ce7a774c54cf85cbc927a49bc6906797a
thiago miziara <thiagonmiziara@gmail.com>
2021-04-20 11:13:04 -0300

    add Sass e configurando estilos globais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,6 @@
+import "../styles/global.scss";
+
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,11 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
+    <>
     <h1>Hello world!</h1>
-   
+    
+   </>
   )
 }
+
+
