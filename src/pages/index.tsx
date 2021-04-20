8ca7e1d0507d2caf3d37737591ef91cee62d0d81
thiago miziara <thiagonmiziara@gmail.com>
2021-04-20 12:57:04 -0300

    add component Player e seus estilos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,7 @@
-import Head from 'next/head'
-import { Header } from '../components/Header'
-
 export default function Home() {
   return (
     <>
-    <Header/>
+    <h1>Index</h1>
     
    </>
   )
