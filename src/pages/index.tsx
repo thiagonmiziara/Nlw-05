7e263f4fbb339a9b89f2f847a7536f611a7f4d32
thiago miziara <thiagonmiziara@gmail.com>
2021-04-21 15:39:27 -0300

    add folder utils & folder services

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,56 @@
-export default function Home() {
+import { GetStaticProps } from "next";
+import {format, parseISO} from "date-fns";
+import ptBR from "date-fns/locale/pt-BR"
+
+import { api } from "../services/api";
+import { convertDurationToTimeString } from "../utils/convertDurationToTimeString";
+
+type Episodes = {
+  id: string;
+  title: string;
+  members: string;
+};
+
+type HomeProps = {
+  episodes: Episodes[];
+};
+
+export default function Home(props: HomeProps) {
   return (
     <>
-    <h1>Index</h1>
-    
-   </>
-  )
+      <h1>Index</h1>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </>
+  );
 }
 
-
-export async function getStaticProps() {
-  
-  const response = await fetch("http://localhost:3333/episodes");
-  const data = await response.json();
-
-  return{
-    props:{
-      episodes: data,
+export const getStaticProps: GetStaticProps = async () => {
+  const {data} = await api.get("episodes", {
+    params: {
+      _limit: 12,
+      _sort: "published_at",
+      _oder: "desc",
     },
-    revalidate:60 * 60 * 8,
-  }
+  });
+  
+  const episodes = data.map(episode =>{
+    return{
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy',{locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url,
+    }
+  })
 
-}
-
-//codigo da aula02 
+  return {
+    props: {
+      episodes,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+};
