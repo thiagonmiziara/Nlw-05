5c7453f76a0f88c13b14ff1db04f94d1fd30eedd
thiago miziara <thiagonmiziara@gmail.com>
2021-04-23 20:20:44 -0300

    finalizando app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { GetStaticProps } from "next";
 import Image from "next/image";
 import Link from "next/link";
+import  Head  from "next/head";
 
 import { format, parseISO } from "date-fns";
 import ptBR from "date-fns/locale/pt-BR";
@@ -34,6 +35,10 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
 
   return (
     <div className={styles.homepage}>
+      <Head>
+        <title>Home | Podcastr</title>
+      </Head>
+
       <section className={styles.latestEpisodes}>
         <h2>Ùltimos lançamentos</h2>
 
